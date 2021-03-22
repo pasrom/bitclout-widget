f6559e11597e86878b634978328e226791ec67c5
pasrom <32713472+pasrom@users.noreply.github.com>
2021-03-22 17:50:27 +0100

    beautify the code

--- bitclout.js
+++ bitclout.js
@@ -3,10 +3,6 @@
 // icon-color: teal; icon-glyph: dollar-sign;
 // Version 0.2.0
 
-const ethGasStationApiUrl = `https://ethgasstation.info/api/ethgasAPI.json`;
-const req = new Request(ethGasStationApiUrl);
-const res = await req.loadJSON();
-
 usdBtcTracker = "https://blockchain.info/ticker"
 getExchangeRate = "https://api.bitclout.com/get-exchange-rate"
 imgBitCloutUrl = "https://i.imgur.com/NAosi4v.png"
@@ -18,7 +14,7 @@ requestUsdBtcTrackerStr = await request.loadJSON()
 request2 = new Request(getExchangeRate);
 requestGetExchangeRate = await request2.loadJSON()
 
-const actUsdPrice =  1e9 / (1e9 / requestGetExchangeRate.SatoshisPerBitCloutExchangeRate / requestUsdBtcTrackerStr.USD.last * 1e8)
+const actUsdPrice = 1e9 / (1e9 / requestGetExchangeRate.SatoshisPerBitCloutExchangeRate / requestUsdBtcTrackerStr.USD.last * 1e8)
 
 const themes = {
   light: {
@@ -40,7 +36,7 @@ if (Device.isUsingDarkAppearance()) {
 }
 
 // load module from url
-const loadModule = (name, version, url) =>  {
+const loadModule = (name, version, url) => {
   return new Promise((callback) => {
     const fm = FileManager.iCloud()
     const modulesPath = fm.joinPath(fm.documentsDirectory(), 'modules/')
@@ -50,7 +46,7 @@ const loadModule = (name, version, url) =>  {
     if (!fm.fileExists(modulePath)) {
       fm.createDirectory(modulePath, true)
     }
-    if (!fm.fileExists(filePath) ) {
+    if (!fm.fileExists(filePath)) {
       const req = new Request(url)
       req.loadString().then(res => {
         fm.writeString(filePath, `${res}`).then(() => {
@@ -69,17 +65,17 @@ module.exports = loadModule
 const BetterRequest = await loadModule("BetterRequest", "2009e6915", betterRequestUrl)
 
 async function getProfile(name) {
-    let req = new BetterRequest("https://api.bitclout.com/get-profiles");
-    req.method = "post";
-    req.json = {
-        PublicKeyBase58Check:"",
-        Username:name,
-        NumToFetch:1,
-        ModerationType:"",
-        FetchUsersThatHODL:false,
-        AddGlobalFeedBool:false
-    };
-    return await req.loadJSON();
+  let req = new BetterRequest("https://api.bitclout.com/get-profiles");
+  req.method = "post";
+  req.json = {
+    PublicKeyBase58Check: "",
+    Username: name,
+    NumToFetch: 1,
+    ModerationType: "",
+    FetchUsersThatHODL: false,
+    AddGlobalFeedBool: false
+  };
+  return await req.loadJSON();
 }
 
 /**
@@ -101,9 +97,9 @@ widget = await createWidget(widgetSize)
 
 if (!config.runsInWidget) {
   if (mediumWidget) {
-      await widget.presentMedium()
+    await widget.presentMedium()
   } else {
-      await widget.presentSmall()
+    await widget.presentSmall()
   }
 }
 
@@ -111,56 +107,54 @@ Script.setWidget(widget)
 Script.complete()
 
 async function createWidget(widgetSize) {
-    const list = new ListWidget()
-    list.backgroundColor = new Color("#ffffff")
-    colorGray = new Color("#777777")
-    textColor = new Color(theme.textColor)
+  const list = new ListWidget()
+  textColor = new Color(theme.textColor)
 
-    if (args.widgetParameter) {
-        parameter = args.widgetParameter
-    } else {
-        parameter = "elonmusk;cryptocobain;RandomTask"
-    }
-    const names = parameter.split(";")
+  if (args.widgetParameter) {
+    parameter = args.widgetParameter
+  } else {
+    parameter = "elonmusk;cryptocobain;RandomTask"
+  }
+  const names = parameter.split(";")
 
-    const stack = list.addStack()
-    stack.layoutVertically()
-    stack.addSpacer()
+  const stack = list.addStack()
+  stack.layoutVertically()
+  stack.addSpacer()
 
-    imgBitclout = await getLogoFromUrl(imgBitCloutUrl)
-    const image = stack.addImage(imgBitclout)
-    image.imageSize = new Size(60, 20)
-    image.tintColor = new Color(theme.picColor)
+  imgBitclout = await getLogoFromUrl(imgBitCloutUrl)
+  const image = stack.addImage(imgBitclout)
+  image.imageSize = new Size(60, 20)
+  image.tintColor = new Color(theme.picColor)
+  stack.addSpacer(3)
+
+  const title = stack.addText("Price ~$" + actUsdPrice.toFixed(2));
+  title.textColor = textColor;
+  title.textOpacity = 0.8;
+  title.font = new Font("Helvetica-Light ", 11);
+  stack.addSpacer(3)
+
+  for (name of names) {
+    let result = await getProfile(name)
+    const coinPriceBitCloutNanos = result.ProfilesFound[0].CoinPriceBitCloutNanos
+    const username = result.ProfilesFound[0].Username
+    const titleUser = stack.addText(username + " ~$" + (coinPriceBitCloutNanos / 1e9 * actUsdPrice).toFixed(2));
+    titleUser.textColor = textColor;
+    titleUser.textOpacity = 0.8;
+    titleUser.minimumScaleFactor = 0.3
+    titleUser.font = new Font("Helvetica-Light ", 11);
     stack.addSpacer(3)
+  }
 
-    const title = stack.addText("Price ~$" + actUsdPrice.toFixed(2));
-    title.textColor = textColor;
-    title.textOpacity = 0.8;
-    title.font = new Font("Helvetica-Light ", 11);
-    stack.addSpacer(3)
+  const date = new Date();
+  currentMinutes = ("0" + date.getMinutes()).slice(-2);
+  currentHours = ("0" + date.getHours()).slice(-2);
+  const lastUpdate = stack.addText("LastUpdate: " + currentHours + ":" + currentMinutes);
+  lastUpdate.textColor = textColor;
+  title.textOpacity = 0.8;
+  lastUpdate.font = new Font("Helvetica-Light", 11);
+  stack.addSpacer()
 
-    for (name of names) {
-        let result = await getProfile(name)
-        const coinPriceBitCloutNanos = result.ProfilesFound[0].CoinPriceBitCloutNanos
-        const username = result.ProfilesFound[0].Username
-        const titleUser = stack.addText(username + " ~$" + (coinPriceBitCloutNanos/1e9*actUsdPrice).toFixed(2));
-        titleUser.textColor = textColor;
-        titleUser.textOpacity = 0.8;
-        titleUser.minimumScaleFactor = 0.3
-        titleUser.font = new Font("Helvetica-Light ", 11);
-        stack.addSpacer(3)
-    }
-
-    const date = new Date();
-    currentMinutes = ("0" + date.getMinutes()).slice(-2);
-    currentHours = ("0" + date.getHours()).slice(-2);
-    const lastUpdate = stack.addText("LastUpdate: " + currentHours + ":" + currentMinutes);
-    lastUpdate.textColor = textColor;
-    title.textOpacity = 0.8;
-    lastUpdate.font = new Font("Helvetica-Light", 11);
-    stack.addSpacer()
-
-    return list
+  return list
 }
 
 /**
